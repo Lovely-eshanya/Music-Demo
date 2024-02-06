bc76db35b86ae5d2973cedcf6310c5d93f1718d7
Lovely-eshanya <146971997+Lovely-eshanya@users.noreply.github.com>
2024-02-06 19:56:40 +0530

    Add files via upload

---
+++ interact.js
@@ -0,0 +1,15 @@
+function f1() {
+    document.getElementById("audio1").play()
+}
+
+function f2() {
+    document.getElementById("audio2").play()
+}
+
+function f3() {
+    document.getElementById("audio3").play()
+}
+
+function f4() {
+    document.getElementById("audio4").play()
+}
